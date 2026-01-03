5d3332a5048d461a02f19b1b3d20d92a97e66bd6
ldevdantesl <buzurg.berdiev@icloud.com>
2026-01-03 00:01:34 -0500

    Initial Commit

---
+++ EduConnect/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  EduConnect
+//
+//  Created by Buzurg Rakhimzoda on 2.01.2026.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+        
+        let initialVC = ViewController()
+        window.rootViewController = initialVC
+        window.makeKeyAndVisible()
+    }
+}
---
+++ EduConnect/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  EduConnect
+//
+//  Created by Buzurg Rakhimzoda on 2.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+
+}
+
